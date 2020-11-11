88b45a006929303aaa1b9b584e38d04ff6a9ab92
Timothy Piggott <piggott291@gmail.com>
2020-11-11 16:41:03 -0500

    Adding config and updating package-lock.json

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+const dbUser = 'team'
+const dbPass = '123Joseph'
+
+
+module.exports = {
+    MongoURI: 'mongodb+srv://' + dbUser + ':' + dbPass + '@localdevtester.0fbhr.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
