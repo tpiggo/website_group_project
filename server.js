18aba48e76d15918f2eaca1adfc2605540e7af56
Louoyar <oyarzabal.louise@gmail.com>
2020-11-11 01:13:52 -0500

    handle routing for subpages

--- server.js
+++ server.js
@@ -5,8 +5,19 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/views/homepage.html");
+    res.render(__dirname + "/views/homepage.ejs");
 });
 
-const PORT = process.env.PORT || 3000;
+app.get('/:pagename', (req, res) => {
+    // console.log('Request received for /' + req.params.pagename + ' - sending file /views/' + req.params.pagename);
+    res.render(__dirname + '/views/' + req.params.pagename + '.ejs');
+});
+
+//If the route isn't recognized
+app.get('*', (req, res) => {
+    console.log(`404 REQUEST NOT RECOGNIZED [${req.url}]`);
+    res.send(`404 REQUEST NOT RECOGNIZED [${req.url}]`);
+});
+
+const PORT = process.env.POsRT || 3000;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
