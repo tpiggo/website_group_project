2f38b1f336d68a39e1bd9b813c284d9559fd3d74
Louise <louise.oyarzabal@mail.mcgill.ca>
2020-11-28 15:27:11 -0500

    updating the dropdowns at every DB modification

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const News = require('../models/News');
 const TechnicalReport = require('../models/TechnicalReport');
 const Posting = require('../models/Posting');
 const Award = require('../models/Award');
+const { Model } = require('mongoose');
 
 router.get('/', (req, res) => {
     res.render('homepage', { logged: req.session.authenticated, username: req.session.username });
@@ -16,31 +17,20 @@ router.get('/', (req, res) => {
 router.get('/dashboard', middleware.isAuthenticated, (req, res) => {
     // Set content
 
-    function getCurrentUser() {
-        return new Promise((resolve, reject) => {
-            User.findOne({ username: req.session.username }, (err, user) => {
-                if (err) {
-                    console.error(err);
-                    reject(err);
-                } else {
-                    resolve(user);
-                }
-            });
-        });
-    }
-    function getAllDataFrom (Model) {
-        return new Promise((resolve, reject) => {
-            Model.find({}, (err, content) => {
-                if (err) {
-                    console.error(err);
-                    reject(err);
-                } else {
-                    resolve(content);
-                }
-            });
-        });
-    }
+    User.findOne({ username: req.session.username }, (err, user) => {
+        if (err) {
+            console.error(err);
+            res.send("error when accessing the User Database");
+        } else {
+            res.render('dashboard', { logged: req.session.authenticated, user: user })
+        }
+    });
 
+});
+
+
+router.get('/api/dashboard-info', middleware.isAuthenticated, (req, res) => {
+    console.log('request received for dropdowns');
     var getCourses = () => getAllDataFrom(Course);
     var getNews = () => getAllDataFrom(News);
     var getEvents = () => getAllDataFrom(Event);
@@ -48,22 +38,32 @@ router.get('/dashboard', middleware.isAuthenticated, (req, res) => {
     var getTech = () => getAllDataFrom(TechnicalReport);
     var getPosting = () => getAllDataFrom(Posting);
 
-    Promise.all([getCurrentUser(), getCourses(), getNews(), getEvents(), getAwards(), getTech(), getPosting()])
+    Promise.all([getCourses(), getNews(), getEvents(), getAwards(), getTech(), getPosting()])
         .then(data => {
-            var user = data[0];
-            var pages ={
-                courses: data[1],
-                news: data[2],
-                events: data[3],
-                awards: data[4],
-                techs: data[5],
-                postings: data[6]
-            };
-        console.log("sending all the data found to user");
-        res.render('dashboard', { logged: req.session.authenticated, user: user, pages: pages})
-    }).catch(err => {
-            console.log(err);
+            console.log('sending all the data to dashboard');
+            res.json({ status: 0, data });
+        }).catch(err => {
+            console.error(err);
+            res.json({ status: 2, response:"error while fecthing data from db"});
         });
 
 });
+
+/**
+ * Get all the data from the collection specified by Model
+ * @param {Model} Model indicate the collection to get data from
+ * @returns {Promise}
+ */
+function getAllDataFrom(Model) {
+    return new Promise((resolve, reject) => {
+        Model.find({}, (err, content) => {
+            if (err) {
+                console.error(err);
+                reject(err);
+            } else {
+                resolve(content);
+            }
+        });
+    });
+}
 module.exports = router;
