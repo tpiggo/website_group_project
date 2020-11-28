1fb6159624845b12731b0c4b2533d6e1c56995cd
Louise <louise.oyarzabal@mail.mcgill.ca>
2020-11-27 19:17:17 -0500

    fix naming error

--- middleware/index.js
+++ middleware/index.js
@@ -7,7 +7,7 @@ middleware.isAuthenticated = (req, res, next) => {
     const title = "Error!";
     content = { "html": "<h1>You do not have access to this page! Please <a href='/users/login'>Login</a> to view this content</h1>" }
     // Render a subpage with the error
-    res.render('subpage', { title, content, menu: [], logged: req.session.authenticated, user: req.session.username });
+    res.render('subpage', { title, content, menu: [], logged: req.session.authenticated, username: req.session.username });
 }
 
 module.exports = middleware;
