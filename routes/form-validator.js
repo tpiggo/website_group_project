1a47df40c376660478c23af4e4e564976c2aac94
Louise <louise.oyarzabal@mail.mcgill.ca>
2020-11-27 21:47:58 -0500

    fixing routing errors

--- routes/form-validator.js
+++ routes/form-validator.js
@@ -35,7 +35,7 @@ router.post('/addTA', middleware.isAuthenticated, (req, res) => {
      */
     function findCourse(title, semester) {
         return new Promise((resolve, reject) => {
-            Course.find({ title: title, termsOffered: semester}, (err, match) => {
+            Course.find({ title: title, termsOffered: semester }, (err, match) => {
                 if (err) {
                     console.error(err);
                     reject({ status: 2, response: 'Error accessing Course database! Try again later!' });
@@ -102,7 +102,7 @@ router.post('/addTA', middleware.isAuthenticated, (req, res) => {
         });
     }
 
-    findCourse(req.body.courseTitle, req.body.semester ).then(found => {
+    findCourse(req.body.courseTitle, req.body.semester).then(found => {
         return findTAPosting(found);
     }).then(post => {
         return createPost(post);
@@ -115,270 +115,212 @@ router.post('/addTA', middleware.isAuthenticated, (req, res) => {
 });
 
 // TODO: Implement this route
-router.post('/addAward', (req, res) => {
+router.post('/addAward', middleware.isAuthenticated, (req, res) => {
     // Auth the session
-    if (req.session.authenticated) {
-        console.log(req.body);
-        const {
-            awardTitle,
-            date,
-            recipient,
-            awardDescription
-        } = req.body;
-        Award.findOne({
-            "$and": [
-                { title: awardTitle },
-                { recipient: recipient }
-            ]
-        })
-            .then(award => {
-                if (award) {
-                    console.log("Award exists!");
-                    res.json({ status: 1, response: 'Award exists! Please modify the existing award!' });
-                } else {
-                    console.log("Creating award");
-                    const pAward = new Award({
-                        title: awardTitle,
-                        date: date,
-                        recipient: recipient,
-                        description: awardDescription,
-                        creator: req.session.username
-                    });
-                    pAward.save()
-                        .then((result) => res.json({
-                            status: 0,
-                            response: 'Award created!',
-                            eventTitle: {
-                                title: awardTitle,
-                                recipient: recipient,
-                                id: result._id
-                            }
-                        }))
-                        .catch(err => {
-                            console.log(err);
-                            res.json({ status: 2, response: 'Error creating award!' });
-                        });
-                }
-            })
-            .catch(err => {
-                console.log(err);
-                res.json({ status: 2, response: 'Error accessing database! Try again later!' });
-            })
-
-        console.log("Sent response");
-    } else {
-        // On err, render 403 err and redirect
-        res.status(403).render();
-        res.redirect('/dashboard');
-    }
-});
-
-// TODO: Implement this route
-router.post('/addNews', (req, res) => {
-    // Auth the session
-    if (req.session.authenticated) {
-        /** 
-         * Not enforcing unique names for the articles.
-         * Return object Id for rendering of article
-         */
-        const {
-            newsType,
-            newsTitle,
-            newsStartdate,
-            newsEnddate,
-            newsContact,
-            newsDescription
-        } = req.body;
-
-        const pArticle = new News({
-            faculty: newsType,
-            title: newsTitle,
-            start: newsStartdate,
-            end: newsEnddate,
-            contact: newsContact,
-            description: newsDescription,
-            creator: req.session.username
-        });
-        // save the article, return the proper response
-        pArticle.save()
-            .then((result) => {
-                res.json({
-                    status: 0,
-                    response: 'News article created!',
-                    article: { title: newsTitle, id: result._id }
+    console.log(req.body);
+    Award.findOne({
+        "$and": [
+            { title: req.body.title },
+            { recipient: req.body.recipient }
+        ]
+    })
+        .then(award => {
+            if (award) {
+                console.log("Award exists!");
+                res.json({ status: 1, response: 'Award exists! Please modify the existing award!' });
+            } else {
+                console.log("Creating award");
+                const pAward = new Award({
+                    ...req.body,
+                    creator: req.session.username
                 });
-            })
-            .catch(err => {
-                console.log(err);
-                res.json({ status: 2, response: 'Error accessing database! Try again later!' });
-            })
-        console.log("Sent response");
-    } else {
-        // On err, render 403 err and redirect
-        res.status(403).render();
-        res.redirect('/dashboard');
-    }
+                pAward.save()
+                    .then((result) => res.json({
+                        status: 0,
+                        response: 'Award created!',
+                        eventTitle: {
+                            title: req.body.title,
+                            recipient: recipient,
+                            id: result._id
+                        }
+                    }))
+                    .catch(err => {
+                        console.log(err);
+                        res.json({ status: 2, response: 'Error creating award!' });
+                    });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.json({ status: 2, response: 'Error accessing database! Try again later!' });
+        })
 });
 
 // TODO: Implement this route
-router.post('/addEvent', (req, res) => {
+router.post('/addNews', middleware.isAuthenticated, (req, res) => {
     // Auth the session
-    if (req.session.authenticated) {
-        console.log(req.body);
-        // Search for an event which already exists
-        const { eventType, eventTitle, eventStartdate, eventEnddate, host, eventDescription } = req.body;
-        Event.findOne({
-            "$and": [
-                { title: eventTitle },
-                { start: eventStartdate },
-                { eventType: eventType }
-            ]
-        })
-            .then(event => {
-                // Check the existance of the event
-                if (event) {
-                    console.log("Event already Exists!");
-                    res.json({ status: 1, response: 'Event exists. Please update it!' });
-                } else {
-                    console.log("No event found!");
-                    // Create the event 
-                    const pEvent = new Event({
-                        eventType: eventType,
-                        title: eventTitle,
-                        start: eventStartdate,
-                        end: eventEnddate,
-                        hostedBy: host,
-                        description: eventDescription,
-                        creator: req.session.username
-                    });
-                    // Save event and handle events
-                    pEvent.save()
-                        .then((result) => res.json({
-                            status: 0,
-                            response: 'Event created!',
-                            event: { eventTitle: eventTitle, id: result._id }
-                        }))
-                        .catch(err => {
-                            console.log(err);
-                            res.json({ status: 2, response: 'Error creating event!' });
-                        });
-                }
-            })
-            .catch(err => {
-                console.log(err);
-                res.json({ status: 2, response: 'Error accessing database! Try again later!' });
+    /** 
+     * Not enforcing unique names for the articles.
+     * Return object Id for rendering of article
+     */
+
+    console.log(req.body);
+    const pArticle = new News({
+        ...req.body,
+        creator: req.session.username
+    });
+    console.log(pArticle);
+
+    // save the article, return the proper response
+    pArticle.save()
+        .then((result) => {
+            res.json({
+                status: 0,
+                response: 'News article created!',
+                article: { title: req.body.title, id: result._id }
             });
-        console.log("Sent response");
-    } else {
-        // On err, render 403 err and redirect
-        res.status(403).render();
-        res.redirect('/dashboard');
-    }
-});
-
-// TODO: Implement this route
-router.post('/addTech', (req, res) => {
-    // Auth the session
-    if (req.session.authenticated) {
-        console.log(req.body);
-        const user = req.session.username;
-        const { techTitle, techContact, techDescription } = req.body
-        // Can have two tech report postings which are essentially identical. Simply checking if 
-        // Someone is trying to readd the same technical report (ie same everything by same creator.) 
-        TechnicalReport.findOne({
-            "$and": [
-                { title: techTitle },
-                { createdBy: user },
-            ]
         })
-            .then(report => {
-                if (report) {
-                    console.log("Found report of same name by same user!");
-                    res.json({ status: 1, response: 'This report has already been submitted!' });
-                } else {
-                    console.log("New report to be submited!");
-                    const pReport = new TechnicalReport({
-                        title: techTitle,
-                        creator: user,
-                        contact: techContact,
-                        description: techDescription,
-                        reportDate: Date.now(),
-                        editors: [],
-                        lastEdited: Date.now()
-                    });
-                    pReport.save()
-                        .then(result => {
-                            console.log("Successfully created new report!");
-                            res.json({
-                                status: 0,
-                                response: 'New technical report created!',
-                                report: { title: techTitle, id: result._id }
-                            });
-                        })
-                        .catch(err => {
-                            console.log(err);
-                            res.json({ status: 2, response: 'Error during creation! Please try again!' });
-                        })
-                }
-            })
-            .catch(err => {
-                console.log(err);
-                res.json({ status: 2, response: 'Error accessing database! Try again later!' });
-            })
-        console.log("Sent response");
-    } else {
-        // On err, render 403 err and redirect
-        res.status(403).render();
-        res.redirect('/dashboard');
-    }
+        .catch(err => {
+            console.log(err);
+            res.json({ status: 2, response: 'Error accessing database! Try again later!' });
+        })
+    console.log("Sent response");
 });
-// TODO: Implement this route
-router.post('/addPosting', (req, res) => {
-    // Auth the session
-    if (req.session.authenticated) {
-        console.log(req.body);
-        // setting params
-        const {
-            postingType,
-            postingTitle,
-            postingStartdate,
-            postingEnddate,
-            postingContact,
-            postingDescription
-        } = req.body;
-        // Just saving a posting.
-        // Should we have database checking? Not sure. Two jobs of the same nature could be created!
-        // __      __
-        //   \(ツ)/
-        const pPosting = new Posting({
-            faculty: postingType,
-            title: postingTitle,
-            start: postingStartdate,
-            end: postingEnddate,
-            contact: postingContact,
-            description: postingDescription,
-            creator: req.session.username
-        });
 
-        pPosting.save()
-            .then(result => {
-                console.log("New Posting created!");
-                res.json({
-                    status: 0,
-                    response: 'Posting created!',
-                    posting: { title: postingTitle, id: result._id }
+// TODO: Implement this route
+router.post('/addEvent', middleware.isAuthenticated, (req, res) => {
+    console.log(req.body);
+    // Search for an event which already exists
+    Event.findOne({
+        "$and": [
+            { title: req.body.title },
+            { start: req.body.start },
+            { eventType: req.body.eventType }
+        ]
+    })
+        .then(event => {
+            // Check the existance of the event
+            if (event) {
+                console.log("Event already Exists!");
+                res.json({ status: 1, response: 'Event exists. Please update it!' });
+            } else {
+                console.log("No event found!");
+                // Create the event 
+                const pEvent = new Event({
+                    ...req.body,
+                    creator: req.session.username
                 });
-            })
-            .catch(err => {
-                console.log(err);
-                res.json({ status: 2, response: 'Error during creation the posting!' });
-            })
-        console.log("Sent response");
-    } else {
-        // On err, render 403 err and redirect
-        res.status(403).render();
-        res.redirect('/dashboard');
-    }
+                // Save event and handle events
+                console.log("pEvent : " + pEvent);
+                pEvent.save()
+                    .then((result) => res.json({
+                        status: 0,
+                        response: 'Event created!',
+                        event: { eventTitle: req.body.title, id: result._id }
+                    }))
+                    .catch(err => {
+                        console.log(err);
+                        res.json({ status: 2, response: 'Error creating event!' });
+                    });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.json({ status: 2, response: 'Error accessing database! Try again later!' });
+        });
+    console.log("Sent response");
+});
+
+// TODO: Implement this route
+router.post('/addTech', middleware.isAuthenticated, (req, res) => {
+    // Auth the session
+    console.log(req.body);
+    const user = req.session.username;
+    const { techTitle, techContact, techDescription } = req.body
+    // Can have two tech report postings which are essentially identical. Simply checking if 
+    // Someone is trying to readd the same technical report (ie same everything by same creator.) 
+    TechnicalReport.findOne({
+        "$and": [
+            { title: req.body.title },
+            { createdBy: user },
+        ]
+    })
+        .then(report => {
+            if (report) {
+                console.log("Found report of same name by same user!");
+                res.json({ status: 1, response: 'This report has already been submitted!' });
+            } else {
+                console.log("New report to be submited!");
+                const pReport = new TechnicalReport({
+                    ...req.body,
+                    creator: user,
+                    reportDate: Date.now(),
+                    editors: [],
+                    lastEdited: Date.now()
+                });
+                pReport.save()
+                    .then(result => {
+                        console.log("Successfully created new report!");
+                        res.json({
+                            status: 0,
+                            response: 'New technical report created!',
+                            report: { title: techTitle, id: result._id }
+                        });
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.json({ status: 2, response: 'Error during creation! Please try again!' });
+                    })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.json({ status: 2, response: 'Error accessing database! Try again later!' });
+        })
+    console.log("Sent response");
+});
+
+// TODO: Implement this route
+router.post('/addPosting', middleware.isAuthenticated, (req, res) => {
+    // Auth the session
+    console.log(req.body);
+    // setting params
+    const {
+        postingType,
+        postingTitle,
+        postingStartdate,
+        postingEnddate,
+        postingContact,
+        postingDescription
+    } = req.body;
+    // Just saving a posting.
+    // Should we have database checking? Not sure. Two jobs of the same nature could be created!
+    // __      __
+    //   \(ツ)/
+    const pPosting = new Posting({
+        faculty: postingType,
+        title: postingTitle,
+        start: postingStartdate,
+        end: postingEnddate,
+        contact: postingContact,
+        description: postingDescription,
+        creator: req.session.username
+    });
+
+    pPosting.save()
+        .then(result => {
+            console.log("New Posting created!");
+            res.json({
+                status: 0,
+                response: 'Posting created!',
+                posting: { title: postingTitle, id: result._id }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.json({ status: 2, response: 'Error during creation the posting!' });
+        })
+    console.log("Sent response");
 });
 
 /** 
@@ -391,7 +333,7 @@ router.post('/addCourse', (req, res) => {
             if (err) {
                 console.log(err);
                 res.json({ status: 2, response: 'Error during creation the course!' });
-            } else{
+            } else {
                 console.log(content);
                 res.json({
                     status: 0,
