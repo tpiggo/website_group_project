9e4df6e78b62cc8596602aa936c3227362604a54
Louise <louise.oyarzabal@mail.mcgill.ca>
2020-12-03 01:23:11 -0500

    added method to common.js

--- common.js
+++ common.js
@@ -16,15 +16,15 @@ common.getAllDataFrom = (Model) => {
             }
         });
     });
- }
+}
 
- /**
+/**
 * Get all the data from the collection specified by Model that has a specific attribute
 * @param {Model} Model indicate the collection to get data from
 * @param {JSON} attribute indicates the attribute the documents need to have
 * @returns {Promise}
 */
-common.getAllDataWith = (Model, attribute) =>{
+common.getAllDataWith = (Model, attribute) => {
     return new Promise((resolve, reject) => {
         Model.find(attribute, (err, content) => {
             if (err) {
@@ -35,7 +35,45 @@ common.getAllDataWith = (Model, attribute) =>{
             }
         });
     });
- }
+}
 
+/** Get and populate all the subpages of a page and extract the content of a specified subpage
+ * @param {String} page indicate the collection to get data from
+ * @param {String} subpage indicates the document to extract the content from
+ * @returns {Promise}   returns a formatted menu of the page to be rendered and the content of the specified subpage
+*/
+const Page = require('./models/Page.js');
+common.getDataOfSubpage = (page, subpage) => {
+    return new Promise((resolve, reject) => {
+        Page
+            .findOne({ path: page })
+            .populate('subpages')
+            .exec((err, data) => {
+                if (err) {
+                    console.logo(err);
+                    reject("Internal error");
+                }
+                else if (data == null) {
+                    reject("404 : this page doesn't exist");
+                }
+                else {
+                    var content = data.subpages.find(e => e.path.includes(subpage));
+                    if (content == undefined) {
+                        reject("404 : this subpage doesn't exist");
+                    }
+                    else {
+                        var menu = []
+                        data.subpages.forEach(element => {
+                            menu.push({ path: element.path, name: element.name, submenu: element.submenu });
+                        });
+                        resolve({ menu: menu, content: content });
+                    }
 
- module.exports = common;
+                }
+            }
+            );
+
+    });
+}
+
+module.exports = common;
