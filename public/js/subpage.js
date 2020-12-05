6f4884086fb72d4fc48e2d2a36b19e3f707f8da0
Louise <louise.oyarzabal@mail.mcgill.ca>
2020-12-05 13:47:24 -0500

    Fixed display errors of navbar

--- public/js/subpage.js
+++ public/js/subpage.js
@@ -4,25 +4,6 @@ nav.classList.add("fixed-top");
 nav.setAttribute("style", "position:sticky !important");
 
 document.body.style.setProperty('--nav-height', (nav.offsetHeight) + 'px');
-//console.log( nav.offsetHeight);
-
-//Disable the nav item corresponding to the 'title' of the page
-var items = nav.getElementsByClassName("nav-item");
-for (var i = 2; i < items.length - 1; i++) {
-    var link = items[i].getElementsByTagName('a');
-    if (link != undefined) {
-        if (link[0].innerText == document.querySelector("#title").innerText) {
-            link[0].classList.add("disabled");
-            break;
-        }
-    }
-}
-
-// // Open and close the left menu with onclick() (hamburger icon)
-// function openMenu() {
-//     document.getElementById('menu').classList.toggle('hidden');
-//     document.getElementById('content').classList.toggle('hidden');
-// }
 
 var submenus = document.getElementsByClassName('submenu');
 
@@ -30,7 +11,6 @@ var submenus = document.getElementsByClassName('submenu');
 for (var i = 0; i < submenus.length; i++) {
     var selected = document.querySelector('#submenu' + submenus[i].id);
     selected.addEventListener('mouseenter', function () {
-        console.warn('louise')
         $(this).find("ul:nth-child(2)").removeClass("hidden");
 
     });
